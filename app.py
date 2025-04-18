583565c96182bc25b6cc781042a5c064cae32a9d
visesh-0401 <viseshchauhan4104@gmail.com>
2025-04-18 10:25:16 +0530

    Fix: Bind to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -73,4 +73,6 @@ def download_csv(domain):
     return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
